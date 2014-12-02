5f7d590795f80d2c7a0f3240f860f6deaf38ef2f
Sharma, Neeraj <neeraj.sharma@sjsu.edu>
2014-12-02 03:26:09 -0800

    Basic impl of BF working, tested, 1.029 % false positives present

--- bloomFilter.py
+++ bloomFilter.py
@@ -7,16 +7,59 @@
 
 import sys
 import hashlib
+import mmh3 # murmurhash: is faster for blooms
+import bitarray
+import BitVector
 
 # set default encoding to utf-8
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-class BloomFilter(self, fp_chance):
+class BloomFilter(object):
 	"""
 	Implement a simple bloom filter in python
 	"""
-	# TODO: decide the data structure to be used for bitmap representation
-	# decide methods and how many and what hashes to be used
+	def __init__(self, m, k):
+		# decide methods and how many and what hashes to be used
+		self.m = m # number of bits in the array
+		self.k = k # number of hashes to be used
+		self.n = 0 # total count of the elemnts inserted in the set, intialized to zero, if this is incremented on add, this will be length of the filter, given elements are not removed
+		self.bv = BitVector.BitVector(size = self.m)
 
+
+	def getBitArrayIndices(self, key):
+		"""
+		hashes the key for k defined,
+		returns the positions in the bit array for this key
+		returns a list of integers as the indices positions
+		"""
+		returnList = []
+		for i in range(1, self.k + 1):
+			returnList.append((hash(key) + i * mmh3.hash(key)) % self.m)
+		#print "Indices list for key: ", key, " is: ", str(returnList)
+		return returnList
+
+	def add(self, key):
+		"""
+		Insert an element to the filter, rest is application insert
+		"""
+		for i in self.getBitArrayIndices(key):
+			self.bv[i] = 1
+		# to check length
+		self.n += 1
+
+	def contains(self, key):
+		"""
+		returns boolean whether element exists in the set or not
+		"""
+		for i in self.getBitArrayIndices(key):
+			if self.bv[i] != 1:
+				return False
+		return True
+
+	def length(self):
+		"""
+		Returns the current size of Bloom filter
+		"""
+		return self.n
---
+++ testBloomFilter.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+#
+# Test the bloom filter imlementation
+# TODO: Benchmark False Positives, test run time,
+# TODO: Compare murmurhash, sha, python hash, cityhash, pyfasthash?
+#
+
+
+from bloomFilter import BloomFilter
+import uuid
+
+def getRandomString():
+	return str(uuid.uuid4())
+
+
+def unitTestBloomFilter():
+	"""
+	Create once instance of BloomFilter and test few keys lookup
+	"""
+	print "\nTesting BloomFilter implementation:\n"
+	bf = BloomFilter(100, 2)
+	bf.add('neeraj')
+	bf.add('database')
+	bf.add('cassandra')
+
+	# test the keys if present
+	if bf.contains('database'):
+		print "Test: OK \tKey: 'database' is present"
+	else:
+		print "Test: Failed \tKey: 'database' is not present"
+	if bf.contains('cassandra'):
+		print "Test: OK \tKey: 'cassandra' is present"
+	else:
+		print "Test: Failed \tKey: 'cassandra' is not present"
+	if bf.contains('anykey'):
+		print "Test: Failed Random Key present"
+	else:
+		print "Test: OK Random Key not present"
+	print "\nUnit Test Completed\n"
+
+def loadTestBloomFilter(n, m, k):
+	"""
+	Create n keys in BF of size m
+	Store 100 keys for retreival, retrieve them
+	Hit 100 random non-existent keys, see how many false positives are received
+	"""
+	print "\nLoad test BloomFilter implementation:\n"
+	existentKeys = []
+
+	# Create a bigger Bloom Filter
+	bf = BloomFilter(m, k)
+	for i in range(0, n):
+		randomString = getRandomString()
+		bf.add(randomString)
+		if (i % 1000) == 0:
+			existentKeys.append(randomString)		
+
+	print "Added ", n, " keys to BloomFilter"
+	print "Current length of BF: " + str(bf.length())
+
+	# Check existent keys
+	for i in existentKeys:
+		if not bf.contains(i):
+			print "Load test failed. Existent key not present"
+	print "Load test OK. All existent keys are present"
+
+	# Check non-existent keys
+	fp = 0
+	for i in range(0, 100000):
+		if bf.contains(getRandomString()):
+			fp += 1
+
+	print "Number of false positives: " + str(fp)
+
+	print "\nLoad test Completed\n"
+
+
+if __name__ == '__main__':
+	unitTestBloomFilter()
+	loadTestBloomFilter(10000, 100000, 10)
