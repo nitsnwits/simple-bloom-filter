e236e060f578c7c6044d2f176a2bdd03f2b8551c
Neeraj Sharma <nitsnwits@gmail.com>
2014-12-01 18:18:57 -0800

    Initial commit of class files

---
+++ bloomFilter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+#
+# Simple implementation of bloom fitler in python
+#
+#
+
+import sys
+import hashlib
+
+# set default encoding to utf-8
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class BloomFilter(self, fp_chance):
+	"""
+	Implement a simple bloom filter in python
+	"""
+	# TODO: decide the data structure to be used for bitmap representation
+	# decide methods and how many and what hashes to be used
+
